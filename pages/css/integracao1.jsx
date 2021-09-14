40454f94997d898de2a243fd960848fb6c337045
Marcelo Schmidt <gmmarceloo@gmail.com>
2021-09-13 22:45:48 -0300

    Changes

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function integracao() {
+    return (
+        <div className="integracao1">
+            <div className="vermelha">TEXTO 01</div>
+            <div className="azul">TEXTO 02</div>
+            <div className="branca">TEXTO 03</div>
+        </div>
+    )
+}
---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,11 @@
+import styles from "./integracao2.module.css"
+
+export default function integracao2() {
+    return (
+        <div id={styles.integracao2}>
+            <div className={styles.vermelha}>TEXTO 01</div>
+            <div className={styles.azul}>TEXTO 02</div>
+            <div className={styles.branca}>TEXTO 03</div>
+        </div>
+    )
+}
