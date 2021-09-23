9b30f6267b259d77d8452977395b28d7f59d4ee5
schmidtmarcelo <gmmarceloo@gmail.com>
2021-09-22 23:42:41 -0300

    exemplo hooks

---
+++ pages/estado/contador.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import ContadorDisplay from "../../components/ContadorDisplay"
+
+export default function contador() {
+    const [valor, setValor] = useState(0)
+
+    /*function calculo(soma) {
+        soma ? setValor(valor + 1) : setValor(valor - 1)
+    }
+    <button onClick={() => calculo(false)}>-</button>
+    <button onClick={() => calculo(true)}>+</button>
+    forma q eu fiz
+    */
+
+    const dec = () => setValor(valor - 1)
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <h1>Contador</h1>
+            <ContadorDisplay numero={valor} />
+            <div>
+                <button onClick={dec}>-</button>
+                <button onClick={() => setValor(valor + 1)}>+</button>
+            </div>
+        </div>
+    )
+}
